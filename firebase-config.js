1e1e00fe0e68e31e38a6288152bce39bb3a3ba82
ellen-happycoding <ellenwang.6002@gmail.com>
2026-03-31 16:45:53 -0700

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.MOOD_TRACKER_FIREBASE = {
-  enabled: true,
-  config: {
-    apiKey: "AIzaSyCpHH-RDlXJXHk1kwNPuLnf4b4ryEiafKI",
-    authDomain: "mood-tracker-2c4e3.firebaseapp.com",
-    projectId: "mood-tracker-2c4e3",
-    appId: "1:964567082459:web:e1ffdfdf059f1a3659b3fe"
-  }
-};
